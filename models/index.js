d55174192d2acf9e4877e862c9ee414ff0851335
yatishprajapatbrandshow <yatish.prajapat@brandshow.in>
2024-12-18 15:28:25 +0530

    resolving the deploy errors

--- models/index.js
+++ models/index.js
@@ -1,15 +1,14 @@
-const College = require("./College.model");
 const Program = require("./Program.model");
 const Accommodation = require ("./Accommodation.model");
 const Stream = require("./Stream.model");
 const ProgramMapped =require("./ProgramMapped.model");
 const Department = require("./Department.model");
-
+const College = require("./College.model");
 module.exports = {
-  College,
   Program,
   Stream,
   Accommodation,
   ProgramMapped,
-  Department
+  Department,
+  College
 }
